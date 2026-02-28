cf0629cf0ff056c108c20d3d9bdfdb85beb61f93
zzzuxt <zuxtmc@gmail.com>
2026-02-28 20:28:16 +0000

    Ping color fixes

--- src/main/java/com/zuxt/pingtag/mixin/EntityRendererMixin.java
+++ src/main/java/com/zuxt/pingtag/mixin/EntityRendererMixin.java
@@ -10,7 +10,6 @@ import net.minecraft.client.render.entity.state.PlayerEntityRenderState;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
 import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
 import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
