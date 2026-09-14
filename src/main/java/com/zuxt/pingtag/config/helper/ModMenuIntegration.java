66c4189901cdd0695e7bb880716a20721c70b8ca
zzzuxt <zuxtmc@gmail.com>
2026-09-14 14:58:43 +0100

    Fixes

--- src/main/java/com/zuxt/pingtag/config/helper/ModMenuIntegration.java
+++ src/main/java/com/zuxt/pingtag/config/helper/ModMenuIntegration.java
@@ -1,4 +1,4 @@
-package com.zuxt.pingtag.helper;
+package com.zuxt.pingtag.config.helper;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
